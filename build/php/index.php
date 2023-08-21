e1b52dce172cfbf569fb437871059ba1d121fddf
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-08-21 19:43:58 +1000

    removed junk

--- build/php/index.php
+++ build/php/index.php
@@ -4,15 +4,7 @@
     <title>LAMP Test Page</title>
 </head>
 <body>
-    <h1>Hello, LAMP Environment!</h1>
-    <p>This is a test page served by the LAMP stack.</p>
-    <?php
-    $currentDate = date("Y-m-d H:i:s");
-    echo "<p>Current server date and time: $currentDate</p>";
-    ?>
-    <?php
-    echo "<p>Server IP address: " . $_SERVER['SERVER_ADDR'] . "</p>";
-    ?>
+   <p>Hello</p> 
 </body>
 </html>
 
