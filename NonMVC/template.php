2c0f1c561fd4aea17b0495b472c6e04a384942bf
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-08-26 22:37:24 +1000

    extracted non-mvc to php so it's easier to read/debug

---
+++ NonMVC/template.php
@@ -0,0 +1,12 @@
+<?php
+$image = '<div class="feature rounded-3 mb-4 mt-n4">
+            <img src="https://wiki.teamfortress.com/w/images/f/f4/Backpack_Sandvich.png"
+                 alt="Heavy\'s Sandvich">
+        </div>';
+$description = '<div>
+                    <h2 class="text-left fs-4 fw-bold">Sandvich</h2>
+                    <h2 class="text-right fs-4 fw-bold">$12</h2>
+                    <p class="mb-0">Bread and Grains group +20 Health, Vegetable Group (2 Services) +40 Health,
+                        Cheese and Diary Group +20 Health, Meat and Poultry Group +20 Health, Bread and Grains Group
+                        +20 Health</p>
+                </div>';
