a8ae448f5b297833272f94b8ea034ef635f3f4af
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-08-26 22:00:06 +1000

    began implementing the database class, fixed a model bug, implemented test model

--- crimsonmvc/app/config/config.php
+++ crimsonmvc/app/config/config.php
@@ -1,5 +1,11 @@
 <?php
+//Database
+define('DB_HOST', 'crimson-mysql-1');
+define('DB_USER', 'root');
+define('DB_PASS', 'password');
+define('DB_NAME', 'cafe');
 
+//App info
 define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'http://localhost/crimsonmvc/');
 define('SITENAME', 'CrimsonMVC');
