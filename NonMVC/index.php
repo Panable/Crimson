37944ac49ffac30e3577227c4ab4b83ca23d3d9f
a0re <102201308+a0re@users.noreply.github.com>
2023-08-26 22:16:08 +1000

    Working in progress layout

---
+++ NonMVC/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- References -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./javascript/main.js" rel="stylesheet">
+    <title>NonMVC - Menu Page</title>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-dark py-lg-4">
+    <div class="col-sm">
+        <p class="text-white"> Testing</p>
+    </div>
+
+    <div class="col-md">
+        <p class="text-white"> Crimson Cafe</p>
+    </div>
+
+    <div class="col-sm">
+        <!-- change this to a list with ul & li -->
+        <p class="text-white">"Profile Login"</p>
+        <p class="text-white">Staff Login</p>
+        <p class="text-white">Menu</p>
+    </div>
+</nav>
+
+<section class="card-section pt-4 mx-4">
+    <div class="row gx-lg-5">
+        <div class="col-lg-6 col-xxl-4 mb-5">
+            <div class="card bg-light border-0 h-100">
+                <div class="shadow p-3 card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                    <div class="feature rounded-3 mb-4 mt-n4">
+                        <img src="https://wiki.teamfortress.com/w/images/f/f4/Backpack_Sandvich.png"
+                             alt="Heavy's Sandvich">
+                    </div>
+
+                    <div>
+                        <h2 class="text-left fs-4 fw-bold">Sandvich</h2>
+                        <h2 class="text-right fs-4 fw-bold"> $12</h2>
+                        <p class="mb-0"> Bread and Grains group + 20 Health, Vegetable Group (2 Services) +40 health,
+                            Cheese and Diary Group +20 Health, Meat and Poultry Group +20 Health, Bread and Grains Group
+                            +20 Health </p>
+                    </div>
+                </div>
+
+
+                <div class="card bg-light border-0 h-75">
+                    <div class="shadow p-3 card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                        <div class="feature rounded-3 mb-4 mt-n4">
+                            <img src="https://wiki.teamfortress.com/w/images/f/f4/Backpack_Sandvich.png" alt="Heavy's Sandvich">
+                        </div>
+
+                        <div>
+                            <h2 class="text-left fs-4 fw-bold">Sandvich</h2>
+                            <h2 class="text-right fs-4 fw-bold"> $12</h2>
+                            <p class="mb-0"> Bread and Grains group + 20 Health, Vegetable Group (2 Services) +40 health, Cheese and Diary Group +20 Health, Meat and Poultry Group +20 Health, Bread and Grains Group +20 Health </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card bg-light border-0 h-75">
+                    <div class="shadow p-3 card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                        <div class="feature rounded-3 mb-4 mt-n4">
+                            <img src="https://wiki.teamfortress.com/w/images/f/f4/Backpack_Sandvich.png" alt="Heavy's Sandvich">
+                        </div>
+
+                        <div>
+                            <h2 class="text-left fs-4 fw-bold">Sandvich</h2>
+                            <h2 class="text-right fs-4 fw-bold"> $12</h2>
+                            <p class="mb-0"> Bread and Grains group + 20 Health, Vegetable Group (2 Services) +40 health, Cheese and Diary Group +20 Health, Meat and Poultry Group +20 Health, Bread and Grains Group +20 Health </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<footer class="pt-4 mx-4">
+
+</footer>
+
+</body>
+</html>
