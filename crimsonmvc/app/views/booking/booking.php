a275bf32f604ab641772bc23286d54e46c1ae1c7
a0re <102201308+a0re@users.noreply.github.com>
2023-11-21 16:47:30 +1100

    Complete: Booking Page

--- crimsonmvc/app/views/booking/booking.php
+++ crimsonmvc/app/views/booking/booking.php
@@ -1,35 +1,34 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<h1>Book your table!</h1>
+<body id="booking-body">
 
-<form action="<?php echo URLROOT . 'booking'  ?>" method="post">
-    <div id="edit-section">
-        <!-- Phone Number Form -->
-        <div class="form-group">
-            <label for="Phone_Number">Phone Number</label>
-            <input type="tel" name="Phone_Number" class="form-control" required>
-        </div>
+<a href="<?php echo URLROOT; ?>" class="text-decoration-none fs-3 d-flex align-items-center my-3" id="icon-home">
+    <span class="material-symbols-outlined fs-2 me-3"> arrow_back </span>
+    <span> Back to Home Page</span>
+</a>
 
-        <!-- Time Form -->
-        <div class="form-group">
-            <label for="Time">Time</label>
-            <input type="time" name="Time" class="form-control" required>
-        </div>
+<div class="row justify-content-center my-5">
+    <div class="col-md-12 col-lg-10 my-5 rounded rounded-3" id="edit-section">
+        <form class="my-5 mx-5" action="<?php echo URLROOT . 'booking' ?>" method="post">
+            <div class="form-group">
+                <p class="ml-2 fs-1 text-center" id="booking-title"> Make a Booking Reservation </p>
+                <hr>
 
-        <!-- Date Form -->
-        <div class="form-group">
-            <label for="Date">Date</label>
-            <input type="date" name="Date" class="form-control" required>
-        </div>
+                <label for="Phone_Number" id="booking-field"  class="fs-4 my-3">Phone Number</label>
+                <input type="tel" name="Phone_Number" class="form-control" required>
 
-        <!-- Guests Form -->
-        <div class="form-group">
-            <label for="Guests">Guests</label>
-            <input type="number" name="Guests" class="form-control" required>
-        </div>
+                <label for="Time" id="booking-field" class="fs-4 my-3">Time</label>
+                <input type="time" name="Time" class="form-control" required>
 
-        <button type="submit" class="btn btn-primary m-3">Submit</button>
+                <label for="Date" id="booking-field" class="fs-4 my-3">Date</label>
+                <input type="date" name="Date" class="form-control" required>
+
+                <label for="Guests" id="booking-field" class="fs-4 my-3">Guests</label>
+                <input type="number" name="Guests" class="form-control" required>
+
+                <button type="submit" class="btn btn-lg my-5 rounded rounded-2 px-5 float-end" id="booking-submit">Submit</button>
+            </div>
+        </form>
     </div>
-</form>
-
-<?php require APPROOT . '/views/inc/footer.php'; ?>
+</div>
+</body>
